6fcb5bfcf6604bab112f9bc92bf5dbea64d4cf1b
vincentzyc <vincentzyc@qq.com>
2020-12-09 09:34:41 +0800

    init🎉 init

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { createApp } from 'vue'
-import App from './App'
+import App from './App.vue'
 import store from './store'
 import Element from './plugins/element'
 
 import '@/assets/css/base.css'
+import '@/assets/css/index.styl';
+import '@/assets/css/widget.styl';
+import '@/assets/css/config.styl';
+import '@/assets/css/themes.styl';
 
-createApp(App).use(store).use(Element).mount('#app')
+const app = createApp(App)
+
+app.config.globalProperties.BASE_URL = process.env.BASE_URL
+
+app.use(store).use(Element).mount('#app')
