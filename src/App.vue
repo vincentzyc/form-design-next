73c920d072149f29dda69adee605d6f44e9d7659
vincentzyc <vincentzyc@qq.com>
2020-12-11 12:32:21 +0800

    🎉init: init

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import Home from "@/components/Home/index.vue"
+import Home from "@/components/Home.vue"
 
 export default defineComponent({
   name: "App",
