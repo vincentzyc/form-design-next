d6f5db95421ed37bfa48cf5da0aa0c8ef5e6c299
vincentzyc <vincentzyc@qq.com>
2020-12-14 09:10:00 +0800

    🎉init: init

--- src/components/WidgetForm/widget-form-list/index.vue
+++ src/components/WidgetForm/widget-form-list/index.vue
@@ -5,7 +5,7 @@
     @click.stop="handleSelectWidget()"
   >
     <i class="el-icon-rank" v-if="Array.isArray(item.list)"></i>
-    <component :is="wgNameMap[item.type]" :item="item" />
+    <component :is="'Wg'+item.type" :item="item" />
     <span
       @click.stop="handleWidgetDelete()"
       class="widget-action-btn widget-action-delete el-icon-delete"
@@ -22,75 +22,19 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, computed, reactive, nextTick } from "vue";
+import { defineComponent, computed, nextTick } from "vue";
 import { useStore } from 'vuex';
 import { deepClone } from '@/utils/deep-clone';
 
-// import WgPhone from './wg-phone'
-// import WgInput from './wg-input'
-// import WgCheckbox from './wg-checkbox'
-// import WgSelect from './wg-select'
-// import WgSwitch from './wg-switch'
-// import WgDate from './wg-date'
-// import WgImgshow from './wg-imgshow'
-// import WgImgslide from './wg-imgslide'
-// import WgButton from './wg-button'
 import WgStaticText from './WgStaticText'
-// import WgSplitLine from './wg-splitLine'
-// import WgHpicker from './wg-hpicker'
-// import WgSms from './wg-sms'
-// import WgWechat from './wg-wechat'
-// import WgMarquee from './wg-marquee'
-// import WgMarqueeSingle from './wg-marquee-single'
-// import WgAgreement from './wg-agreement'
-// import WgRandomCode from './wg-random-code'
-// import WgVideoPlay from './wg-video-play'
 
 export default defineComponent({
   name: "WidgetFormList",
   components: {
-    // WgPhone,
-    // WgInput,
-    // WgCheckbox,
-    // WgSelect,
-    // WgSwitch,
-    // WgDate,
-    // WgImgshow,
-    // WgImgslide,
-    // WgButton,
     WgStaticText,
-    // WgSplitLine,
-    // WgHpicker,
-    // WgSms,
-    // WgWechat,
-    // WgMarquee,
-    // WgMarqueeSingle,
-    // WgAgreement,
-    // WgRandomCode,
-    // WgVideoPlay,
-    // WgFormList: () => import('./wg-formlist')
 
     //下面是按需加载，组件多的话可选择此方式
-    // WgPhone: () => import('./wg-phone'),
-    // WgInput: () => import('./wg-input'),
-    // WgCheckbox: () => import('./wg-checkbox'),
-    // WgSelect: () => import('./wg-select'),
-    // WgSwitch: () => import('./wg-switch'),
-    // WgDate: () => import('./wg-date'),
-    // WgImgshow: () => import('./wg-imgshow'),
-    // WgImgslide: () => import('./wg-imgslide'),
-    // WgButton: () => import('./wg-button'),
-    // WgStaticText: () => import('./wg-statictext'),
-    // WgSplitLine: () => import('./wg-splitLine'),
-    // WgHpicker: () => import('./wg-hpicker'),
-    // WgSms: () => import('./wg-sms'),
-    // WgWechat: () => import('./wg-wechat'),
-    // WgMarquee: () => import('./wg-marquee'),
-    // WgMarqueeFixed: () => import('./wg-marquee-fixed'),
-    // WgAgreement: () => import('./wg-agreement'),
-    // WgRandomCode: () => import('./wg-random-code'),
-    // WgVideoPlay: () => import('./wg-video-play'),
-    // WgFormList: () => import('./wg-formlist')
+    // WgStaticText: () => import('./WgStaticText'),
   },
   props: {
     item: {
@@ -113,28 +57,6 @@ export default defineComponent({
   },
   emits: ['update:data', 'emptied'],
   setup(props, { emit }) {
-    const wgNameMap = reactive({
-      phone: 'WgPhone', // 手机
-      input: 'WgInput', // 输入框
-      checkbox: 'WgCheckbox', // 选择框
-      select: 'WgSelect', // 下拉选择
-      switch: 'WgSwitch', // 开关
-      date: 'WgDate', // 日期选择
-      imgShow: 'WgImgshow', // 图片展示
-      imgSlide: 'WgImgslide', // 图片轮播
-      button: 'WgButton', // 按钮
-      staticText: 'WgStaticText', // 文本描述
-      splitLine: 'WgSplitLine', // 分割线
-      hPicker: 'WgHpicker', // 横向滑动自动选择
-      sms: 'WgSms', // 短信发送 
-      wechat: 'WgWechat', // 微信关注 
-      marquee: 'WgMarquee', // 跑马灯 
-      marqueeSingle: 'WgMarqueeSingle', // 跑马灯-单个
-      agreement: 'WgAgreement', // 用户协议
-      randomCode: 'WgRandomCode', //随机码
-      videoPlay: 'WgVideoPlay',//视频播放
-      formList: 'WgFormList', // 表单内容区 
-    })
     const store = useStore()
     const selectWg = computed(() => store.state.selectWg)
     const clonebtn = computed(() => {
@@ -177,7 +99,7 @@ export default defineComponent({
       store.commit('setSelectWg', props.data[props.index + 1])
     }
     return {
-      wgNameMap, selectWg,
+      selectWg,
       clonebtn, wgViewStyle,
       handleSelectWidget, handleWidgetDelete, handleWidgetClone
     }
