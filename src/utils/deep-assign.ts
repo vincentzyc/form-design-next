59d3ce2eff1665475012e0039c659a29c72ed2fa
vincentzyc <vincentzyc@qq.com>
2020-12-14 10:55:50 +0800

    🚅perf: deep-assign

--- src/utils/deep-assign.ts
+++ src/utils/deep-assign.ts
@@ -4,24 +4,19 @@ type ObjectIndex = Record<string, any>;
 
 const { hasOwnProperty } = Object.prototype;
 
-function assignKey(to: ObjectIndex, from: ObjectIndex, key: string) {
-  const val = from[key];
-
-  if (!isDef(val)) {
-    return;
-  }
-
-  if (!hasOwnProperty.call(to, key) || !isObject(val)) {
-    to[key] = val;
-  } else {
-    // eslint-disable-next-line no-use-before-define
-    to[key] = deepAssign(Object(to[key]), from[key]);
-  }
-}
-
 export function deepAssign(to: ObjectIndex, from: ObjectIndex): ObjectIndex {
   Object.keys(from).forEach((key) => {
-    assignKey(to, from, key);
+    const val = from[key];
+  
+    if (!isDef(val)) {
+      return;
+    }
+  
+    if (!hasOwnProperty.call(to, key) || !isObject(val)) {
+      to[key] = val;
+    } else {
+      to[key] = deepAssign(Object(to[key]), from[key]);
+    }
   });
 
   return to;
