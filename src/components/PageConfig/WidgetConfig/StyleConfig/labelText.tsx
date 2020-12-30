c39f6e6dcc86b3f0a41ce3f506d7cd338f32eb12
vincentzyc <vincentzyc@qq.com>
2020-12-30 09:04:03 +0800

    🎉init: init

---
+++ src/components/PageConfig/WidgetConfig/StyleConfig/labelText.tsx
@@ -0,0 +1,21 @@
+const labelText = {
+  percentWidth: "宽度(%)",
+  pxWidth: "宽度(px)",
+  pxHeight: "高度(px)",
+  marginleft: "左边距(px)",
+  margintop: "上边距(px)",
+  margin: "外边距（上 右 下 左 、空格隔开）",
+  padding: "内边距（上 右 下 左 、空格隔开）",
+  backgroundColor: "背景颜色",
+  color: "文字颜色",
+  textAlign: "对齐方式",
+  lineHeight: "文本行高(px)",
+  fontSize: "文字大小(px)",
+  borderColor: "边框色",
+  borderRadius: "圆角",
+  borderWidth: "边框大小",
+  borderStyle: "边框类型",
+  borderTopWidth: "上边框大小"
+}
+
+export default labelText
