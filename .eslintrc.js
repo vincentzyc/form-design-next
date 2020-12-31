4502ca23897af3e0d49b1df7229e50ea6f110149
vincentzyc <vincentzyc@qq.com>
2020-12-31 12:24:39 +0800

    🎉init: init

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,5 +15,6 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    // 'vue/no-mutating-props': 'off',
   }
 }
