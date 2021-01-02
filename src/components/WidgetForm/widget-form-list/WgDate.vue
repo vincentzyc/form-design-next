38e69634e4e037b6a63bdedb480ff30a4dd36cd9
vincentzyc <vincentzyc@qq.com>
2021-01-02 10:49:35 +0800

    ✨feat: WgDate

---
+++ src/components/WidgetForm/widget-form-list/WgDate.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="wg-item wg-padding" :class="[item.label.labelPosition==='top'?'flex-column':'align-middle']" :style="item.style">
+    <div class="wg-title" :style="{width:item.label.labelWidth}">{{item.label.labelTitle}}</div>
+    <div class="flex-auto">
+      <input type="date" :value="item.value" class="wg-input flex-auto" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    item: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
