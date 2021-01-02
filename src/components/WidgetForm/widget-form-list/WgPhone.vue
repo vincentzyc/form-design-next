4af6e0faaa4153c4299d485cbc719c5c0dd0843d
vincentzyc <vincentzyc@qq.com>
2021-01-02 10:51:35 +0800

    ✨feat: WgPhone

--- src/components/WidgetForm/widget-form-list/WgPhone.vue
+++ src/components/WidgetForm/widget-form-list/WgPhone.vue
@@ -1,16 +1,24 @@
 <template>
-  <div class="wg-phone wg-padding" :style="item.style">
-    <div class="wg-item" :class="[item.label.labelPosition==='top'?'flex-column':'align-middle']">
-      <div class="wg-title" :style="{width:item.label.labelWidth}" v-show="item.showLabel">{{item.label.labelTitle}}</div>
+  <div :style="item.style" class="wg-phone wg-padding">
+    <div :class="[item.label.labelPosition==='top'?'flex-column':'align-middle']" class="wg-item">
+      <div
+        :style="{width:item.label.labelWidth}"
+        class="wg-title"
+        v-show="item.showLabel"
+      >{{item.label.labelTitle}}</div>
       <div class="flex-auto">
-        <input class="wg-input" v-model="item.value" :placeholder="item.placeholder">
+        <input :placeholder="item.placeholder" :value="item.value" class="wg-input" />
       </div>
     </div>
-    <div class="wg-item" :class="[item.label.labelPosition==='top'?'flex-column':'align-middle']" v-if="item.showCode">
-      <div class="wg-title flex-none" v-show="item.showLabel" :style="{width:item.label.labelWidth}">验证码</div>
+    <div
+      :class="[item.label.labelPosition==='top'?'flex-column':'align-middle']"
+      class="wg-item"
+      v-if="item.showCode"
+    >
+      <div :style="{width:item.label.labelWidth}" class="wg-title flex-none" v-show="item.showLabel">验证码</div>
       <div class="flex flex-auto">
-        <input placeholder="验证码" class="wg-input">
-        <button class="getVerCode-btn" :style="item.style.btnStyle">获取验证码</button>
+        <input class="wg-input" placeholder="验证码" />
+        <button :style="item.style.btnStyle" class="getVerCode-btn">获取验证码</button>
       </div>
     </div>
   </div>
