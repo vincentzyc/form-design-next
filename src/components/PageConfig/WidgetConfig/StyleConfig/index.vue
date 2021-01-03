174eaa005b00a9b38a1556feb0794e7c20d1ef0b
vincentzyc <vincentzyc@qq.com>
2021-01-03 13:19:31 +0800

    ✨feat: StyleItemConfig

--- src/components/PageConfig/WidgetConfig/StyleConfig/index.vue
+++ src/components/PageConfig/WidgetConfig/StyleConfig/index.vue
@@ -7,19 +7,7 @@
     </el-form-item>
     <el-form-item label="背景图片" v-if="selectWg.hasOwnProperty('backgroundImage')">
       <!-- <FileUpload :value.sync="selectWg.backgroundImage" /> -->
-    </el-form-item>
-
-    <el-form-item label="使用图片按钮" v-if="selectWg.style.hasOwnProperty('isImgBtn')">
-      <el-switch v-model="selectWg.style.isImgBtn"></el-switch>
-    </el-form-item>
-    <el-form-item label="按钮样式：" v-if="selectWg.style.hasOwnProperty('btnStyle')">
-      <el-form-item label="上传按钮图片" v-if="selectWg.style.isImgBtn">
-        <!-- <FileUpload :value.sync="selectWg.style.value" /> -->
-        <p>上传按钮图片</p>
-      </el-form-item>
-      <div v-else>
-        <Style v-model:style="selectWg.style.btnStyle" labelPrefix="按钮"/>
-      </div>
+      <p>TODO 上传图片</p>
     </el-form-item>
 
     <el-form-item label="每项宽度" v-if="selectWg.hasOwnProperty('itemWidth')">
@@ -29,19 +17,21 @@
       <el-input-number :min="1" size="small" v-model="selectWg.showNumber" />
     </el-form-item>
     <template v-if="selectWg.hasOwnProperty('pickerStyle')">
-      <el-form-item label="选中后文字颜色">
-        <el-color-picker :predefine="predefineColors" show-alpha v-model="selectWg.pickerStyle.color" />
-      </el-form-item>
-      <el-form-item label="选中后文字大小(px)">
-        <el-input-number
-          :max="30"
-          :min="10"
-          @change="val=>selectWg.pickerStyle.fontSize = `${val}px`"
-          size="small"
-          v-model="selectWg.pickerStyle.fontsize"
-        />
-      </el-form-item>
+      <Style labelPrefix="选中后" v-model:style="selectWg.pickerStyle" />
     </template>
+
+    <el-form-item label="使用图片按钮" v-if="selectWg.style.hasOwnProperty('isImgBtn')">
+      <el-switch v-model="selectWg.style.isImgBtn"></el-switch>
+    </el-form-item>
+    <el-form-item label="按钮样式：" v-if="selectWg.style.hasOwnProperty('btnStyle')">
+      <el-form-item label="上传按钮图片" v-if="selectWg.style.isImgBtn">
+        <!-- <FileUpload :value.sync="selectWg.style.value" /> -->
+        <p>TODO 上传图片</p>
+      </el-form-item>
+      <div v-else>
+        <Style labelPrefix="按钮" v-model:style="selectWg.style.btnStyle" />
+      </div>
+    </el-form-item>
   </section>
 </template>
 
