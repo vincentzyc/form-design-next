6c743006ba42fd0fc73ab3566a0d9da2b8ed6d6a
vincentzyc <vincentzyc@qq.com>
2021-01-23 13:52:27 +0800

    ⚡perf: @typescript-eslint/explicit-module-boundary-types

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,6 +15,7 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    "@typescript-eslint/explicit-module-boundary-types": "off"
     // 'vue/no-mutating-props': 'off',
   }
 }
