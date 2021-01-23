5fcde88cd954cbe379dda9287634c047335482e9
vincentzyc <vincentzyc@qq.com>
2021-01-23 13:51:44 +0800

    ⚡perf: isLink

--- src/utils/validate/link.ts
+++ src/utils/validate/link.ts
@@ -1,4 +1,6 @@
 export function isLink(value: string): boolean {
+  if (typeof value !== 'string') return false
+  if (value.includes('weixin://')) return true
   const pattern = /^(((ht|f)tps?):\/\/)?[\w-]+(\.[\w-]+)+([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?$/
   const isLink = pattern.test(value);
   return isLink;
