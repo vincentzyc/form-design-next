44ecf780095b5af9d87390a78ee824205c2435aa
vincentzyc <vincentzyc@qq.com>
2021-01-24 10:09:47 +0800

    ✨feat: WgMarqueeSingle

---
+++ src/components/WidgetForm/widget-form-list/WgMarqueeSingle.tsx
@@ -0,0 +1,76 @@
+import { defineComponent, watch, ref, onMounted, reactive, nextTick, onBeforeUnmount } from "vue";
+import { deepClone } from '@/utils/deep-clone';
+export default defineComponent({
+  name: "WgMarqueeSingle",
+  props: {
+    item: {
+      type: Object,
+      required: true
+    }
+  },
+  setup(props) {
+    const { item } = reactive(props)
+
+    const scrollList = ref([])
+    const scrollTimeId = ref(0)
+    const animateTimeId = ref(0)
+    const animate = ref(false)
+
+    const scroll = () => {
+      if (animate.value) return;
+      clearTimeout(animateTimeId.value);
+      animate.value = true;
+      animateTimeId.value = window.setTimeout(() => {
+        scrollList.value.push(scrollList.value[0]);
+        scrollList.value.shift();
+        animate.value = false;
+      }, 500);
+    }
+
+    watch(() => item.textList, (newValue) => {
+      scrollList.value = deepClone(newValue) as []
+    }, { immediate: true, deep: true })
+
+    watch(() => item.durationTime, (newValue) => {
+      clearInterval(scrollTimeId.value);
+      scrollTimeId.value = window.setInterval(scroll, newValue * 1000);
+    }, { immediate: true, deep: true })
+
+    const renderScrollList = () => scrollList.value.map((text: Record<string, any>, i) => (
+      <li
+        key={i}
+        class={['flex align-middle scroll-item', { 'anim': animate.value && i === 0 }]}
+        style={{ marginTop: animate.value && i === 0 ? `-${item.style.height}` : '' }}
+      >
+        {/* 如需要支持自定义头像，使用 img 标签方式 */}
+        {/* <img
+          src={require(`@/assets/img/${text.sex==='man'?'manhead.png':'womanhead.png'}`)}
+          alt="头像"
+          class="userimg flex-none"
+        /> */}
+        <span
+          class={['userimg flex-none', text.sex === 'man' ? 'man' : 'woman']}
+          style={{ width: item.style.height }}
+        ></span>
+        <span class="scroll-text txtover white-space-pre">{text.text}</span>
+      </li>
+    ))
+
+    onMounted(async () => {
+      await nextTick(() => {
+        scrollTimeId.value = window.setInterval(scroll, item.durationTime * 1000);
+      })
+    })
+    onBeforeUnmount(() => {
+      window.clearInterval(scrollTimeId.value);
+      window.clearTimeout(animateTimeId.value);
+    })
+    return () => (
+      <div class="wg-marquee-single flex flex-center">
+        <ul style={{ ...item.style, borderRadius: item.style.height }} class="scroll-wrapper">
+          {renderScrollList()}
+        </ul>
+      </div>
+    )
+  }
+})
