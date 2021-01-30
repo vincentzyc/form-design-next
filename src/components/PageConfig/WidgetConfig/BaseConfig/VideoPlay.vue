b83def61fcc4e43492c7c399c438993564ce8b81
vincentzyc <vincentzyc@qq.com>
2021-01-30 15:14:28 +0800

    ✨feat: VideoPlay

---
+++ src/components/PageConfig/WidgetConfig/BaseConfig/VideoPlay.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <el-form-item label="视频地址">
+      <el-input size="small" v-model="selectWg.videoAttr.src" />
+    </el-form-item>
+    <el-form-item label="封面图片地址">
+      <el-input size="small" v-model="selectWg.videoAttr.poster" />
+    </el-form-item>
+    <el-form-item label="是否自动播放">
+      <el-switch v-model="selectWg.videoAttr.autoplay"></el-switch>
+    </el-form-item>
+    <el-form-item label="是否循环播放">
+      <el-switch v-model="selectWg.videoAttr.loop"></el-switch>
+    </el-form-item>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, computed } from "vue";
+import { useStore } from "vuex";
+
+export default defineComponent({
+  setup() {
+    const store = useStore()
+    const selectWg = computed(() => store.state.selectWg)
+    return {
+      selectWg
+    }
+  }
+})
+</script>
