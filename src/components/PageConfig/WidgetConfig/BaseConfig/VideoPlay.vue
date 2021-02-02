6b0cc395c6e9a0646b1ab85ede3ee11bd189fc98
vincentzyc <vincentzyc@qq.com>
2021-02-02 13:52:53 +0800

    ⚡perf: 代码优化

--- src/components/PageConfig/WidgetConfig/BaseConfig/VideoPlay.vue
+++ src/components/PageConfig/WidgetConfig/BaseConfig/VideoPlay.vue
@@ -1,18 +1,16 @@
 <template>
-  <div>
-    <el-form-item label="视频地址">
-      <el-input size="small" v-model="selectWg.videoAttr.src" />
-    </el-form-item>
-    <el-form-item label="封面图片地址">
-      <el-input size="small" v-model="selectWg.videoAttr.poster" />
-    </el-form-item>
-    <el-form-item label="是否自动播放">
-      <el-switch v-model="selectWg.videoAttr.autoplay"></el-switch>
-    </el-form-item>
-    <el-form-item label="是否循环播放">
-      <el-switch v-model="selectWg.videoAttr.loop"></el-switch>
-    </el-form-item>
-  </div>
+  <el-form-item label="视频地址">
+    <el-input size="small" v-model="selectWg.videoAttr.src" />
+  </el-form-item>
+  <el-form-item label="封面图片地址">
+    <el-input size="small" v-model="selectWg.videoAttr.poster" />
+  </el-form-item>
+  <el-form-item label="是否自动播放">
+    <el-switch v-model="selectWg.videoAttr.autoplay"></el-switch>
+  </el-form-item>
+  <el-form-item label="是否循环播放">
+    <el-switch v-model="selectWg.videoAttr.loop"></el-switch>
+  </el-form-item>
 </template>
 
 <script lang="ts">
--- src/components/PageConfig/WidgetConfig/BaseConfig/Wechat.vue
+++ src/components/PageConfig/WidgetConfig/BaseConfig/Wechat.vue
@@ -1,21 +1,19 @@
 <template>
-  <div>
-    <el-form-item label="微信名称">
-      <el-input placeholder="请输入您的微信名称" v-model="selectWg.value"></el-input>
-    </el-form-item>
-    <el-form-item label="微信账号">
-      <el-input placeholder="请输入您的微信账号" v-model="selectWg.wechatName"></el-input>
-    </el-form-item>
-    <el-form-item label="微信头像">
-      <FileUpload v-model:modelValue="selectWg.logo" />
-    </el-form-item>
-    <el-form-item label="显示微信头像">
-      <el-switch v-model="selectWg.showLogo"></el-switch>
-    </el-form-item>
-    <el-form-item label="显示微信名称">
-      <el-switch v-model="selectWg.showName"></el-switch>
-    </el-form-item>
-  </div>
+  <el-form-item label="微信名称">
+    <el-input placeholder="请输入您的微信名称" v-model="selectWg.value"></el-input>
+  </el-form-item>
+  <el-form-item label="微信账号">
+    <el-input placeholder="请输入您的微信账号" v-model="selectWg.wechatName"></el-input>
+  </el-form-item>
+  <el-form-item label="微信头像">
+    <FileUpload v-model:modelValue="selectWg.logo" />
+  </el-form-item>
+  <el-form-item label="显示微信头像">
+    <el-switch v-model="selectWg.showLogo"></el-switch>
+  </el-form-item>
+  <el-form-item label="显示微信名称">
+    <el-switch v-model="selectWg.showName"></el-switch>
+  </el-form-item>
 </template>
 
 <script lang="ts">
