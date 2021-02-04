e1434889536aee4a9f6969fcd08c926ed815bf21
vincentzyc <vincentzyc@qq.com>
2021-02-04 08:56:00 +0800

    ✨feat: Marquee

--- src/components/PageConfig/WidgetConfig/BaseConfig/marquee.vue
+++ src/components/PageConfig/WidgetConfig/BaseConfig/marquee.vue
@@ -1,69 +1,80 @@
 <template>
-  <div>
-    <el-form-item label="滚动方向" v-if="selectWg.hasOwnProperty('direction')">
-      <el-radio-group v-model="selectWg.direction" size="mini">
-        <el-radio-button label="up">向上</el-radio-button>
-        <el-radio-button label="left">向左</el-radio-button>
-      </el-radio-group>
-    </el-form-item>
-    <el-form-item label="滚动时间（s）" v-if="selectWg.hasOwnProperty('durationTime')">
-      <el-input-number v-model="selectWg.durationTime" :min="1" :max="60" size="small" />
-    </el-form-item>
-    <el-form-item label="内容高度(px)">
-      <el-input-number
-        v-model="selectWg.style.Height"
-        :min="10"
-        :max="200"
-        size="small"
-        @change="v=>selectWg.style.height=v+'px'"
-      />
-    </el-form-item>
-    <el-form-item label="使用图片内容" v-if="selectWg.hasOwnProperty('isImgBtn')">
-      <el-switch v-model="selectWg.isImgBtn"></el-switch>
-    </el-form-item>
-    <el-form-item label="上传图片" v-if="selectWg.isImgBtn&&selectWg.hasOwnProperty('imgUrl')">
-      <FileUpload :value.sync="selectWg.imgUrl" />
-    </el-form-item>
-    <el-form-item label="选项（可使用空格分列）" v-if="!selectWg.isImgBtn&&selectWg.hasOwnProperty('textList')">
-      <Draggable tag="ul" :list="selectWg.textList" :group="{ name:'options'}" ghostClass="ghost" handle=".move-icon">
-        <li v-for="(item, index) in selectWg.textList" :key="index">
-          <div class="flex align-middle">
+  <el-form-item label="滚动方向" v-if="selectWg.hasOwnProperty('direction')">
+    <el-radio-group size="mini" v-model="selectWg.direction">
+      <el-radio-button label="up">向上</el-radio-button>
+      <el-radio-button label="left">向左</el-radio-button>
+    </el-radio-group>
+  </el-form-item>
+  <el-form-item label="滚动时间（s）" v-if="selectWg.hasOwnProperty('durationTime')">
+    <el-input-number :max="60" :min="1" size="small" v-model="selectWg.durationTime" />
+  </el-form-item>
+  <el-form-item label="内容高度(px)">
+    <el-input-number
+      :max="200"
+      :min="10"
+      @change="v=>selectWg.style.height=v+'px'"
+      size="small"
+      v-model="selectWg.style.Height"
+    />
+  </el-form-item>
+  <el-form-item label="使用图片内容" v-if="selectWg.hasOwnProperty('isImgBtn')">
+    <el-switch v-model="selectWg.isImgBtn"></el-switch>
+  </el-form-item>
+  <el-form-item label="上传图片" v-if="selectWg.isImgBtn&&selectWg.hasOwnProperty('imgUrl')">
+    <FileUpload v-model:modelValue="selectWg.imgUrl" />
+  </el-form-item>
+  <el-form-item label="选项（可使用空格分列）" v-if="!selectWg.isImgBtn&&selectWg.hasOwnProperty('textList')">
+    <Draggable
+      :group="{ name:'options'}"
+      ghostClass="ghost"
+      handle=".move-icon"
+      item-key="index"
+      tag="ul"
+      v-model="selectWg.textList"
+    >
+      <template #item="{index}">
+        <li>
+          <div class="flex align-middle mg-b10">
             <el-input size="small" v-model="selectWg.textList[index]"></el-input>
             <i class="el-icon-menu move-icon"></i>
-            <i class="el-icon-delete delect-icon" @click="handleOptionsRemove(index)"></i>
+            <i @click="handleOptionsRemove(index)" class="el-icon-delete delect-icon"></i>
           </div>
         </li>
-      </Draggable>
-      <div style="margin-left: 22px;">
-        <el-button type="text" @click="handleAddOption()">添加选项</el-button>
-      </div>
-    </el-form-item>
-  </div>
+      </template>
+    </Draggable>
+    <div style="margin-left: 22px;">
+      <el-button @click="handleAddOption()" type="text">添加选项</el-button>
+    </div>
+  </el-form-item>
 </template>
 
-<script>
+<script lang="ts">
+import FileUpload from '@/components/base/FileUpload.vue'
+import { defineComponent, computed } from "vue";
 import Draggable from 'vuedraggable'
-export default {
+import { useStore } from "vuex";
+
+export default defineComponent({
   components: {
-    Draggable
+    FileUpload, Draggable
   },
-  props: {
-    selectWg: {
-      type: Object,
-      required: true
+  setup() {
+    const store = useStore()
+    const selectWg = computed(() => store.state.selectWg)
+    const handleOptionsRemove = (index: number) => {
+      selectWg.value.textList.splice(index, 1)
     }
-  },
-  methods: {
-    handleOptionsRemove(index) {
-      this.selectWg.textList.splice(index, 1)
-    },
-    handleAddOption() {
-      let newItem =
-        this.selectWg.textList.length > 0
-          ? this.selectWg.textList[this.selectWg.textList.length - 1]
+    const handleAddOption = () => {
+      const newItem =
+        selectWg.value.textList.length > 0
+          ? selectWg.value.textList[selectWg.value.textList.length - 1]
           : ''
-      this.selectWg.textList.push(newItem)
+      selectWg.value.textList.push(newItem)
+    }
+    return {
+      selectWg,
+      handleOptionsRemove, handleAddOption
     }
   }
-}
+})
 </script>
