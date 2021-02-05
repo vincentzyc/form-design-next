f9d191667bb4d5faf74ea0ad1504f0b3478221a0
vincentzyc <vincentzyc@qq.com>
2021-02-05 08:53:36 +0800

    🐳chore: vue.config

---
+++ vue.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  publicPath: "./",
+  outputDir: "docs",
+  productionSourceMap: false,
+  // configureWebpack: {
+  //   // 以下库使用cdn，不会被打包
+  //   externals: process.env.NODE_ENV === 'production' ? {
+  //     "vue": "Vue",
+  //     "vue-router": 'VueRouter',
+  //     "vuex": "Vuex",
+  //     "vuedraggable": "vuedraggable",
+  //     "compressorjs": "Compressor"
+  //   } : {}
+  // },
+  // chainWebpack: config => {
+  //   config.plugins.delete('preload')
+  //   config.plugins.delete('prefetch')
+  //   config.module
+  //     .rule('images')
+  //     .use('url-loader')
+  //     .loader('url-loader')
+  //     .tap(options => Object.assign(options, { limit: 10240 }))
+  // },
+  devServer: {
+    port: 9000
+  }
+};
