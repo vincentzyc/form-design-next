8632fed83920e7e30333a652dcc9e58463e33c4c
vincentzyc <vincentzyc@qq.com>
2021-02-11 10:27:05 +0800

    ✨feat: MarqueeSingle

--- src/components/PageConfig/WidgetConfig/BaseConfig/MarqueeSingle.vue
+++ src/components/PageConfig/WidgetConfig/BaseConfig/MarqueeSingle.vue
@@ -1,44 +1,42 @@
 <template>
-  <div>
-    <el-form-item label="滚动间隔（秒）" v-if="selectWg.hasOwnProperty('durationTime')">
-      <el-input-number
-        :max="6"
-        :min="1"
-        :step="0.5"
-        size="small"
-        step-strictly
-        v-model="selectWg.durationTime"
-      />
-    </el-form-item>
+  <el-form-item label="滚动间隔（秒）" v-if="selectWg.hasOwnProperty('durationTime')">
+    <el-input-number
+      :max="6"
+      :min="1"
+      :step="0.5"
+      size="small"
+      step-strictly
+      v-model="selectWg.durationTime"
+    />
+  </el-form-item>
 
-    <el-form-item label="选项" v-if="selectWg.hasOwnProperty('textList')">
-      <Draggable
-        :group="{ name:'options'}"
-        ghostClass="ghost"
-        handle=".move-icon"
-        item-key="index"
-        tag="ul"
-        v-model="selectWg.textList"
-      >
-        <template #item="{index}">
-          <li>
-            <div class="flex align-middle">
-              <el-input size="small" v-model="selectWg.textList[index].text"></el-input>
-              <el-radio-group class="flex-none mg-l5" size="mini" v-model="selectWg.textList[index].sex">
-                <el-radio-button label="man">男</el-radio-button>
-                <el-radio-button label="woman">女</el-radio-button>
-              </el-radio-group>
-              <i class="el-icon-menu move-icon"></i>
-              <i @click="handleOptionsRemove(index)" class="el-icon-delete delect-icon"></i>
-            </div>
-          </li>
-        </template>
-      </Draggable>
-      <div class="mg-l10">
-        <el-button @click="handleAddOption()" type="text">添加</el-button>
-      </div>
-    </el-form-item>
-  </div>
+  <el-form-item label="选项" v-if="selectWg.hasOwnProperty('textList')">
+    <Draggable
+      :group="{ name:'options'}"
+      ghostClass="ghost"
+      handle=".move-icon"
+      item-key="index"
+      tag="ul"
+      v-model="selectWg.textList"
+    >
+      <template #item="{index}">
+        <li>
+          <div class="flex align-middle mg-b10">
+            <el-input size="small" v-model="selectWg.textList[index].text"></el-input>
+            <el-radio-group class="flex-none mg-l5" size="mini" v-model="selectWg.textList[index].sex">
+              <el-radio-button label="man">男</el-radio-button>
+              <el-radio-button label="woman">女</el-radio-button>
+            </el-radio-group>
+            <i class="el-icon-menu move-icon"></i>
+            <i @click="handleOptionsRemove(index)" class="el-icon-delete delect-icon"></i>
+          </div>
+        </li>
+      </template>
+    </Draggable>
+    <div class="mg-l10">
+      <el-button @click="handleAddOption()" type="text">添加</el-button>
+    </div>
+  </el-form-item>
 </template>
 
 <script lang="ts">
