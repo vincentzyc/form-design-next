a69123197ee198753d04f4efa3df8f56d319357a
vincentzyc <vincentzyc@qq.com>
2021-03-15 10:08:11 +0800

    ✨feat: ImgSlideConfig

--- src/components/PageConfig/WidgetConfig/BaseConfig/imgslide.vue
+++ src/components/PageConfig/WidgetConfig/BaseConfig/imgslide.vue
@@ -2,37 +2,40 @@
   <el-form-item label="轮播图片设置">
     <Draggable
       :group="{ name:'slideList'}"
-      :list="selectWg.value"
       ghostClass="ghost"
       handle=".move-icon"
+      itemKey="index"
       tag="ul"
+      v-model="selectWg.value"
     >
-      <li :key="index" style="border:1px dashed #999" v-for="(item, index) in selectWg.value">
-        <div class="pd10">
-          <div class="relative flex flex-center">
-            <FileUpload v-model:modelValue="item.image" />
-            <div class="absolute-top-right">
-              <i class="el-icon-menu move-icon"></i>
-              <i @click="handleSlideRemove(index)" class="el-icon-delete delect-icon"></i>
+      <template #item="{element}">
+        <li style="border:1px dashed #999">
+          <div class="pd10">
+            <div class="relative flex flex-center">
+              <FileUpload v-model:modelValue="element.image" />
+              <div class="absolute-top-right">
+                <i class="el-icon-menu move-icon"></i>
+                <i @click="handleSlideRemove(index)" class="el-icon-delete delect-icon"></i>
+              </div>
+            </div>
+            <div class="flex">
+              <span class="flex-none">图片地址：</span>
+              <el-input placeholder="请输入图片地址" size="mini" v-model="element.image"></el-input>
+            </div>
+            <div class="flex">
+              <span class="flex-none">跳转地址：</span>
+              <el-input placeholder="请输入跳转地址" size="mini" v-model="element.url"></el-input>
             </div>
           </div>
-          <div class="flex">
-            <span class="flex-none">图片地址：</span>
-            <el-input placeholder="请输入图片地址" size="mini" v-model="item.image"></el-input>
-          </div>
-          <div class="flex">
-            <span class="flex-none">跳转地址：</span>
-            <el-input placeholder="请输入跳转地址" size="mini" v-model="item.url"></el-input>
-          </div>
-        </div>
-      </li>
+        </li>
+      </template>
     </Draggable>
     <div class="text-center">
       <el-button @click="handleAddSlide()" type="text">添加图片</el-button>
     </div>
   </el-form-item>
   <el-form-item label="图片高度(px)">
-    <el-input-number :max="300" :min="100" size="small" v-model="selectWg.style.height" />
+    <el-input-number :min="80" size="small" v-model="selectWg.style.height" />
   </el-form-item>
   <el-form-item>
     <template #label>
@@ -41,7 +44,14 @@
         <i class="el-icon-info fs12 mg-l10"></i>
       </el-tooltip>
     </template>
-    <el-input-number :max="10000" :min="1000" size="small" v-model="selectWg.interval" />
+    <el-input-number
+      :max="10000"
+      :min="1000"
+      :step="500"
+      size="small"
+      step-strictly
+      v-model="selectWg.interval"
+    />
   </el-form-item>
 </template>
 
@@ -52,7 +62,7 @@ import { useStore } from "vuex";
 import FileUpload from '@/components/base/FileUpload.vue'
 
 export default defineComponent({
-  name:"ImgSlideConfig",
+  name: "ImgSlideConfig",
   components: {
     FileUpload, Draggable
   },
@@ -60,10 +70,10 @@ export default defineComponent({
     const store = useStore()
     const selectWg = computed(() => store.state.selectWg)
     const handleSlideRemove = (index: number) => {
-      selectWg.value.splice(index, 1)
+      selectWg.value.value.splice(index, 1)
     }
     const handleAddSlide = () => {
-      selectWg.value.push({
+      selectWg.value.value.push({
         url: "https://www.baidu.com/",
         image: "https://www.baidu.com/img/bd_logo1.png"
       })
