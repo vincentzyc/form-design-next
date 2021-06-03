6a9c677cc06c66b78a502b1ac45b5d9d69398a30
vincentzyc <vincentzyc@qq.com>
2021-06-03 09:48:25 +0800

    🐎 ci: no-console: off

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,7 +12,7 @@ module.exports = {
     ecmaVersion: 2020
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-console': 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     "@typescript-eslint/explicit-module-boundary-types": "off"
