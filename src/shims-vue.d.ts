b0dba9281723e161dca5c2b0ee025e0d4e863c5d
vincentzyc <vincentzyc@qq.com>
2021-08-23 11:39:54 +0800

    ⚡ perf: ComponentCustomProperties

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,7 +1,3 @@
-import mitt from 'mitt';
-
-const emitter = mitt();
-
 declare module '*.vue' {
   import type { DefineComponent } from 'vue'
   const component: DefineComponent<{}, {}, any>
@@ -9,6 +5,8 @@ declare module '*.vue' {
 }
 
 declare module '@vue/runtime-core' {
+  import mitt from 'mitt';
+  const emitter = mitt();
   export interface ComponentCustomProperties {
     $bus: typeof emitter
     BASE_URL: typeof process.env
