c57fcc7fa7cce65895478b57acb3f4bf077d2882
vincentzyc <vincentzyc@qq.com>
2021-08-23 11:20:34 +0800

    ✨ feat: script setup

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,5 +1,16 @@
+import mitt from 'mitt';
+
+const emitter = mitt();
+
 declare module '*.vue' {
   import type { DefineComponent } from 'vue'
   const component: DefineComponent<{}, {}, any>
   export default component
+}
+
+declare module '@vue/runtime-core' {
+  export interface ComponentCustomProperties {
+    $bus: typeof emitter
+    BASE_URL: typeof process.env
+  }
 }
