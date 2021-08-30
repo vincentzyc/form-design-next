0795e247c6f09055f7466ed8d084a0d7f8b4697f
vincentzyc <vincentzyc@qq.com>
2021-08-30 09:39:25 +0800

    🐳 chore: 组件按需加载

--- src/components/PageConfig/WidgetConfig/BaseConfig/index.vue
+++ src/components/PageConfig/WidgetConfig/BaseConfig/index.vue
@@ -7,9 +7,8 @@
   </section>
 </template>
 
-<script lang="ts" setup>
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import { computed, getCurrentInstance } from "vue";
+<script lang="ts">
+import { defineComponent, defineAsyncComponent, computed, getCurrentInstance } from "vue";
 import { useStore } from "vuex";
 import { hasKey } from "@/utils/index";
 import Common from "./Common.vue";
@@ -17,20 +16,37 @@ import FixedConfig from "./FixedConfig.vue"
 import PopupConfig from "./PopupConfig.vue"
 import MarqueeSingle from "./MarqueeSingle.vue"
 import Marquee from "./Marquee.vue"
-import Wechat from "./Wechat.vue"
+// import Wechat from "./Wechat.vue"
 import Agreement from "./Agreement.vue"
 import ImgShow from "./ImgShow.vue"
 import ImgSlide from "./ImgSlide.vue"
 import Sms from "./Sms.vue"
 import VideoPlay from "./VideoPlay.vue"
 
-//按需加载
-// Common: () => import('./Common.vue'),
-
-const vm: any = getCurrentInstance()?.proxy
-
-const store = useStore()
-const selectWg = computed(() => store.state.selectWg)
-
-const hadComponent = computed(() => hasKey(vm.$options.components, selectWg.value.type))
+export default defineComponent({
+  components: {
+    Common,
+    MarqueeSingle,
+    Marquee,
+    FixedConfig,
+    PopupConfig,
+    // Wechat,
+    Agreement,
+    ImgShow,
+    ImgSlide,
+    Sms,
+    VideoPlay,
+    //按需加载
+    Wechat: defineAsyncComponent(() => import('./Wechat.vue'))
+  },
+  setup() {
+    const vm: any = getCurrentInstance()?.proxy
+    const store = useStore()
+    const selectWg = computed(() => store.state.selectWg)
+    const hadComponent = computed(() => hasKey(vm.$options.components, selectWg.value.type))
+    return {
+      selectWg, hadComponent
+    }
+  }
+})
 </script>
