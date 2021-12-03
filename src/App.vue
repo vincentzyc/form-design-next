ee151476f9c20f26d7e913e0cfb3792c484af8e6
vincentzyc <vincentzyc@qq.com>
2021-12-03 18:21:32 +0800

    ✨ feat: add  zh-cn

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,24 @@
 <template>
-  <Home />
+  <el-config-provider :locale="locale">
+    <Home />
+  </el-config-provider>
 </template>
 
-<script lang="ts" setup>
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { ElConfigProvider } from 'element-plus'
+import zhCn from 'element-plus/lib/locale/lang/zh-cn'
 import Home from "@/components/Home.vue"
+
+export default defineComponent({
+  name: "app",
+  components: {
+    ElConfigProvider, Home
+  },
+  setup() {
+    return {
+      locale: zhCn,
+    }
+  }
+})
 </script>
