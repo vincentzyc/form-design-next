90e0b336cad4456f5a57a012569320d5d41036c6
vincentzyc <vincentzyc@qq.com>
2021-12-31 18:05:27 +0800

    🐳 chore: pinia test

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,18 @@
 <template>
-  <el-config-provider :locale="locale">
+  <el-config-provider :locale="zhCn">
     <Home />
   </el-config-provider>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+export default defineComponent({
+  name: "app"
+})
+</script>
+
+<script lang="ts" setup>
 import { ElConfigProvider } from 'element-plus'
 import zhCn from 'element-plus/lib/locale/lang/zh-cn'
 import Home from "@/components/Home.vue"
-
-export default defineComponent({
-  name: "app",
-  components: {
-    ElConfigProvider, Home
-  },
-  setup() {
-    return {
-      locale: zhCn,
-    }
-  }
-})
 </script>
