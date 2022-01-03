c47524e7694d63df26466d6e0ef6a4a4ac9635cb
vincentzyc <vincentzyc@qq.com>
2022-01-03 21:53:24 +0800

    🎉 init: init

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -4,13 +4,3 @@ declare module '*.vue' {
   const component: DefineComponent<{}, {}, any>
   export default component
 }
-
-import '@vue/runtime-core'
-declare module '@vue/runtime-core' {
-  import mitt from 'mitt';
-  const emitter = mitt();
-  export interface ComponentCustomProperties {
-    $bus: typeof emitter
-    BASE_URL: typeof process.env
-  }
-}
