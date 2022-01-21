fef228c5e4301e9f4e3b3ec43602ab448cb17f76
vincentzyc <vincentzyc@qq.com>
2022-01-21 12:28:34 +0800

    🧪 test: pinia test

--- src/components/PageConfig/WidgetConfig/BaseConfig/Common.vue
+++ src/components/PageConfig/WidgetConfig/BaseConfig/Common.vue
@@ -1,75 +1,77 @@
 <template>
-  <el-form-item label="选择控件" v-if="hasKey(selectWg, 'fieldTypes')">
-    <el-select
-      @change="selectfield(selectWg.apiKey, fieldTypes[selectWg.fieldTypes])"
-      placeholder="请选择"
-      v-model="selectWg.apiKey"
-    >
-      <el-option
-        :key="item.value"
-        :label="item.label"
-        :value="item.value"
-        v-for="item in fieldTypes[selectWg.fieldTypes]"
-      ></el-option>
-    </el-select>
-  </el-form-item>
-  <el-form-item label="是否必填/选" v-if="hasKey(selectWg, 'isRequired')">
-    <el-switch v-model="selectWg.isRequired"></el-switch>
-  </el-form-item>
-  <el-form-item label="是否显示标签" v-if="hasKey(selectWg, 'showLabel')">
-    <el-switch v-model="selectWg.showLabel"></el-switch>
-  </el-form-item>
-  <el-form-item label="是否发送验证码" v-if="hasKey(selectWg, 'showCode')">
-    <el-switch v-model="selectWg.showCode"></el-switch>
-  </el-form-item>
-  <el-form-item label="提示内容" v-if="hasKey(selectWg, 'placeholder')">
-    <el-input v-model="selectWg.placeholder"></el-input>
-  </el-form-item>
-  <el-form-item label="文本内容" v-if="selectWg.type === 'StaticText'">
-    <Editor v-model:modelValue="selectWg.value" />
-  </el-form-item>
-  <el-form-item label="跳转地址(空或格式错误都不会跳转)" v-if="hasKey(selectWg, 'link')">
-    <el-input @change="checkLink" v-model="selectWg.link"></el-input>
-  </el-form-item>
-  <el-form-item label="是否单选" v-if="hasKey(selectWg, 'isRadio')">
-    <el-switch @change="isRadio" v-model="selectWg.isRadio"></el-switch>
-  </el-form-item>
-  <el-form-item label="默认选中" v-if="selectWg.type === 'switch'">
-    <el-switch v-model="selectWg.value"></el-switch>
-  </el-form-item>
-  <el-form-item label="按钮文字" v-if="hasKey(selectWg, 'btnText')">
-    <el-input v-model="selectWg.btnText"></el-input>
-  </el-form-item>
-  <el-form-item label="按钮类型" v-if="hasKey(selectWg, 'btnType')">
-    <el-select filterable placeholder="请选择" v-model="selectWg.btnType">
-      <el-option :key="item.value" :label="item.label" :value="item.value" v-for="item in selectWg.btnTypes"></el-option>
-    </el-select>
-  </el-form-item>
+  <div v-if="selectWg">
+    <el-form-item label="选择控件" v-if="hasKey(selectWg, 'fieldTypes')">
+      <el-select
+        @change="selectfield(selectWg?.apiKey, fieldTypes[selectWg?.fieldTypes])"
+        placeholder="请选择"
+        v-model="selectWg.apiKey"
+      >
+        <el-option
+          :key="item.value"
+          :label="item.label"
+          :value="item.value"
+          v-for="item in fieldTypes[selectWg.fieldTypes]"
+        ></el-option>
+      </el-select>
+    </el-form-item>
+    <el-form-item label="是否必填/选" v-if="hasKey(selectWg, 'isRequired')">
+      <el-switch v-model="selectWg.isRequired"></el-switch>
+    </el-form-item>
+    <el-form-item label="是否显示标签" v-if="hasKey(selectWg, 'showLabel')">
+      <el-switch v-model="selectWg.showLabel"></el-switch>
+    </el-form-item>
+    <el-form-item label="是否发送验证码" v-if="hasKey(selectWg, 'showCode')">
+      <el-switch v-model="selectWg.showCode"></el-switch>
+    </el-form-item>
+    <el-form-item label="提示内容" v-if="hasKey(selectWg, 'placeholder')">
+      <el-input v-model="selectWg.placeholder"></el-input>
+    </el-form-item>
+    <el-form-item label="文本内容" v-if="selectWg.type === 'StaticText'">
+      <Editor v-model:modelValue="selectWg.value" />
+    </el-form-item>
+    <el-form-item label="跳转地址(空或格式错误都不会跳转)" v-if="hasKey(selectWg, 'link')">
+      <el-input @change="checkLink" v-model="selectWg.link"></el-input>
+    </el-form-item>
+    <el-form-item label="是否单选" v-if="hasKey(selectWg, 'isRadio')">
+      <el-switch @change="isRadio" v-model="selectWg.isRadio"></el-switch>
+    </el-form-item>
+    <el-form-item label="默认选中" v-if="selectWg.type === 'switch'">
+      <el-switch v-model="selectWg.value"></el-switch>
+    </el-form-item>
+    <el-form-item label="按钮文字" v-if="hasKey(selectWg, 'btnText')">
+      <el-input v-model="selectWg.btnText"></el-input>
+    </el-form-item>
+    <el-form-item label="按钮类型" v-if="hasKey(selectWg, 'btnType')">
+      <el-select filterable placeholder="请选择" v-model="selectWg.btnType">
+        <el-option :key="item.value" :label="item.label" :value="item.value" v-for="item in selectWg.btnTypes"></el-option>
+      </el-select>
+    </el-form-item>
 
-  <el-form-item label="选项" v-if="hasKey(selectWg, 'options')">
-    <Draggable
-      :animation="100"
-      :group="{ name: 'options' }"
-      ghostClass="ghost"
-      handle=".move-icon"
-      item-key="index"
-      tag="ul"
-      v-model="selectWg.options"
-    >
-      <template #item="{ index }">
-        <li>
-          <div class="flex align-middle">
-            <el-input size="mini" v-model="selectWg.options[index]"></el-input>
-            <i class="el-icon-menu move-icon"></i>
-            <i @click="handleOptionsRemove(index)" class="el-icon-delete delect-icon"></i>
-          </div>
-        </li>
-      </template>
-    </Draggable>
-    <div style="margin-left: 22px;">
-      <el-button @click="handleAddOption()" type="text">添加选项</el-button>
-    </div>
-  </el-form-item>
+    <el-form-item label="选项" v-if="hasKey(selectWg, 'options')">
+      <Draggable
+        :animation="100"
+        :group="{ name: 'options' }"
+        ghostClass="ghost"
+        handle=".move-icon"
+        item-key="index"
+        tag="ul"
+        v-model="selectWg.options"
+      >
+        <template #item="{ index }">
+          <li>
+            <div class="flex align-middle">
+              <el-input size="mini" v-model="selectWg.options[index]"></el-input>
+              <i class="el-icon-menu move-icon"></i>
+              <i @click="handleOptionsRemove(index)" class="el-icon-delete delect-icon"></i>
+            </div>
+          </li>
+        </template>
+      </Draggable>
+      <div style="margin-left: 22px;">
+        <el-button @click="handleAddOption()" type="text">添加选项</el-button>
+      </div>
+    </el-form-item>
+  </div>
 </template>
 
 <script lang="ts" setup>
