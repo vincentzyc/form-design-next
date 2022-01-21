49d86a2b82575dd7515ff85f897cf19f57c66a15
vincentzyc <vincentzyc@qq.com>
2022-01-22 02:09:40 +0800

    🧪 test: pinia test

--- src/components/PageConfig/WidgetConfig/BaseConfig/Wechat.vue
+++ src/components/PageConfig/WidgetConfig/BaseConfig/Wechat.vue
@@ -1,37 +1,28 @@
 <template>
-  <el-form-item label="微信名称">
-    <el-input placeholder="请输入您的微信名称" v-model="selectWg.value"></el-input>
-  </el-form-item>
-  <el-form-item label="微信账号">
-    <el-input placeholder="请输入您的微信账号" v-model="selectWg.wechatName"></el-input>
-  </el-form-item>
-  <el-form-item label="微信头像">
-    <FileUpload v-model:modelValue="selectWg.logo" />
-  </el-form-item>
-  <el-form-item label="显示微信头像">
-    <el-switch v-model="selectWg.showLogo"></el-switch>
-  </el-form-item>
-  <el-form-item label="显示微信名称">
-    <el-switch v-model="selectWg.showName"></el-switch>
-  </el-form-item>
+  <template v-if="selectWg">
+    <el-form-item label="微信名称">
+      <el-input placeholder="请输入您的微信名称" v-model="selectWg.value"></el-input>
+    </el-form-item>
+    <el-form-item label="微信账号">
+      <el-input placeholder="请输入您的微信账号" v-model="selectWg.wechatName"></el-input>
+    </el-form-item>
+    <el-form-item label="微信头像">
+      <FileUpload v-model:modelValue="selectWg.logo" />
+    </el-form-item>
+    <el-form-item label="显示微信头像">
+      <el-switch v-model="selectWg.showLogo"></el-switch>
+    </el-form-item>
+    <el-form-item label="显示微信名称">
+      <el-switch v-model="selectWg.showName"></el-switch>
+    </el-form-item>
+  </template>
 </template>
 
-<script lang="ts">
+<script lang="ts" setup>
 import FileUpload from '@/components/base/FileUpload.vue'
-import { defineComponent, computed } from "vue";
-import { useStore } from "vuex";
+import { useMainStore } from '@/pinia'
+import { storeToRefs } from "pinia";
 
-export default defineComponent({
-  name: "WechatConfig",
-  components: {
-    FileUpload
-  },
-  setup() {
-    const store = useStore()
-    const selectWg = computed(() => store.state.selectWg)
-    return {
-      selectWg
-    }
-  }
-})
+const mainStore = useMainStore()
+const { selectWg } = storeToRefs(mainStore)
 </script>
