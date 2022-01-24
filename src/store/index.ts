a966519d3a3d5243806f89d569fd5d4db6e9f1f6
vincentzyc <vincentzyc@qq.com>
2022-01-24 11:06:51 +0800

    🧪 test: pinia test

--- src/store/index.ts
+++ src/store/index.ts
@@ -19,27 +19,11 @@ export default createStore({
         alertImg: '',
         alertLink: ''
       }
-    },
-    selectWg: {},
-    // dragWg: {},
-    // configTab: 'page'
+    }
   },
   mutations: {
-    // setDragWg(state, payload) {
-    //   state.dragWg = payload;
-    // },
     setPageData(state, payload) {
       state.pageData = payload;
-    },
-    setSelectWg(state, payload) {
-      state.selectWg = payload;
     }
-    // setConfigTab(state, payload) {
-    //   state.configTab = payload;
-    // }
   }
-  // actions: {
-  // },
-  // modules: {
-  // }
 })
