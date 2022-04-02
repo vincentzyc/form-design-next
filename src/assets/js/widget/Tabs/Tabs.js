238ad0a823831cd9e4824bbb6513d3eed398d00a
vincentzyc <vincentzyc@qq.com>
2022-04-02 21:14:51 +0800

    ✨ feat: add wgTabs

---
+++ src/assets/js/widget/Tabs/Tabs.js
@@ -0,0 +1,24 @@
+export default [{
+  type: "Tabs",
+  name: "Tabs标签",
+  value: "0",
+  wgClassName: "wg-formlist widget-active",
+  tabsList: [{
+    title: "标签一",
+    name: "标签内容区",
+    list: []
+  }, {
+    title: "标签二",
+    name: "标签内容区",
+    list: []
+  }],
+  style: {
+    margin: "0px 20px 0px 20px",
+    borderradius: 10,
+    borderRadius: '10px',
+    borderColor: "#409eff",
+    borderwidth: 3,
+    borderWidth: '3px',
+    borderStyle: "solid"
+  }
+}]
