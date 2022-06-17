879bbf3628f19b715c8c7a73499662bc96fdef3c
vincentzyc <vincentzyc@qq.com>
2022-06-17 08:54:44 +0800

    ✨ feat: 新增倒计时组件

---
+++ src/components/WidgetForm/WidgetFormList/WgCountDown.tsx
@@ -0,0 +1,139 @@
+import { defineComponent, reactive, ref } from "vue";
+
+import { CountDownTypes } from "@/assets/js/widget/CountDown/CountDown.js"
+const SECOND = 1000;
+const MINUTE = 60 * SECOND;
+const HOUR = 60 * MINUTE;
+const DAY = 24 * HOUR;
+
+export default defineComponent({
+  name: "WgCountDown",
+  props: {
+    item: {
+      type: Object,
+      required: true
+    }
+  },
+  // data() {
+  //   return {
+  //     timer: 0,
+  //     isFinished: true,
+  //     parseTime: {
+  //       days: 0,
+  //       hours: 0,
+  //       minutes: 0,
+  //       seconds: 0
+  //     }
+  //   }
+  // },
+  // watch: {
+  //   'item.countDownType'(nval) {
+  //     if (nval) {
+  //       clearInterval(this.timer)
+  //       this.initCountDown()
+  //     }
+  //   },
+  //   'item.endTime'(nTime) {
+  //     if (nTime) {
+  //       clearInterval(this.timer)
+  //       this.initCountDown()
+  //     }
+  //   }
+  // },
+  setup(props) {
+    const { item } = reactive(props)
+
+    const isFinished = ref(true)
+    const timer = ref<NodeJS.Timer | null>(null)
+
+    const parseTime = ref({
+      days: 0,
+      hours: 0,
+      minutes: 0,
+      seconds: 0
+    })
+
+    function getTime(time: number) {
+      const days = Math.floor(time / DAY);
+      const hours = Math.floor(time % DAY / HOUR);
+      const minutes = Math.floor(time % HOUR / MINUTE);
+      const seconds = Math.floor(time % MINUTE / SECOND);
+      return {
+        days: days,
+        hours: hours,
+        minutes: minutes,
+        seconds: seconds
+      };
+    }
+    function initEndTimeCountDown() {
+      isFinished.value = item.endTime < Date.now()
+      if (isFinished.value) return``
+      parseTime.value = getTime(item.endTime - Date.now())
+      timer.value = setInterval(() => {
+        isFinished.value = item.endTime < Date.now()
+        parseTime.value = getTime(item.endTime - Date.now())
+      }, 1000);
+    }
+    function initDayLoopCountDown() {
+      isFinished.value = false
+      const today = new Date().toLocaleDateString()
+      const todayTimeStamp = new Date(today).getTime()
+      const tomorrowTimeStamp = todayTimeStamp + 24 * 60 * 60 * 1000
+      parseTime.value = getTime(tomorrowTimeStamp - Date.now())
+      timer.value = setInterval(() => {
+        parseTime.value = getTime(tomorrowTimeStamp - Date.now())
+      }, 1000);
+    }
+    function initCountDown() {
+      switch (item.countDownType) {
+        case CountDownTypes.endTime:
+          initEndTimeCountDown()
+          break;
+        case CountDownTypes.dayLoop:
+          initDayLoopCountDown()
+          break;
+        default:
+          initEndTimeCountDown()
+          break;
+      }
+    }
+    initCountDown()
+    return () => (
+      <div style={{ ...item.style, backgroundImage: `url(${item.style.backgroundImage})` }} class="wg-countdown">
+        <p class="flex flex-center">
+          <span class="countdown-title">{item.title}</span>
+          {
+            isFinished.value ?
+              <span class="cred">请选择倒计时结束时间</span> :
+              <span class="cred">倒计时</span> 
+              // <div>
+              //   <span style={item.timeStyle} class="countdown-timeblock" v-if="parseTime.days>0">{{ parseTime.days }}</span>
+              //   <span style={item.unitStyle} class="countdown-colon" v-if="parseTime.days>0">天</span>
+              //   <span
+              //     style={item.timeStyle}
+              //     class="countdown-timeblock"
+              //     v-if="parseTime.days>0||parseTime.hours>0"
+              //   >{{ parseTime.hours > 9 ? parseTime.hours : '0' + parseTime.hours }}</span>
+              //   <span style={item.unitStyle} class="countdown-colon" v-if="parseTime.days>0||parseTime.hours>0">时</span>
+              //   <span
+              //     style={item.timeStyle}
+              //     class="countdown-timeblock"
+              //     v-if="parseTime.days>0||parseTime.hours>0||parseTime.minutes>0"
+              //   >{{ parseTime.minutes > 9 ? parseTime.minutes : '0' + parseTime.minutes }}</span>
+              //   <span
+              //     style={item.unitStyle}
+              //     class="countdown-colon"
+              //     v-if="parseTime.days>0||parseTime.hours>0||parseTime.minutes>0"
+              //   >分</span>
+              //   <span
+              //     style={item.timeStyle}
+              //     class="countdown-timeblock"
+              //   >{{ parseTime.seconds > 9 ? parseTime.seconds : '0' + parseTime.seconds }}</span>
+              //   <span style={item.unitStyle} class="countdown-colon">秒</span>
+              // </div>
+          }
+        </p>
+      </div >
+    )
+  }
+})
