87da5d11f33ac0c42908a8b79baccc178e64b670
vincentzyc <vincentzyc@qq.com>
2022-08-14 21:23:36 +0800

    🧪 test: jest test

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="hello">
+    <h1>haha{{ msg }}</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'HelloWorld',
+  props: {
+    msg: String,
+  },
+});
+</script>
