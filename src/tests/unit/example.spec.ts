8193beec432ea5a8c55febfa2dd4a6e908e1d23d
vincentzyc <vincentzyc@qq.com>
2022-08-14 21:23:40 +0800

    🧪 test: jest test

---
+++ src/tests/unit/example.spec.ts
@@ -0,0 +1,13 @@
+import { shallowMount } from '@vue/test-utils'
+import HelloWorld from '@/components/HelloWorld.vue'
+
+describe('HelloWorld.vue', () => {
+  it('renders props.msg when passed', () => {
+    const msg = 'new message'
+    const wrapper = shallowMount(HelloWorld, {
+      props: { msg }
+    })
+    console.log(wrapper);
+    expect(wrapper.text()).toMatch(msg)
+  })
+})
