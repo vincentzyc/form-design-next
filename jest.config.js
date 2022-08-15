fb02574386f461f4a660fe9ef15a06fab1a154d0
vincentzyc <vincentzyc@qq.com>
2022-08-15 10:26:34 +0800

    🧪 test: jest test

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,6 @@
 module.exports = {
-  preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel'
+  preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
+  testEnvironmentOptions: {
+    url: 'https://h5.junjue888.com/h5/index.html?id=6061699948802140225&abtest=1&pid=37289'
+  }
 }
--- src/utils/__tests__/index.spec.ts
+++ src/utils/__tests__/index.spec.ts
@@ -1,4 +1,4 @@
-import { sum, hasKey } from '../index'
+import { sum, hasKey, getType, getUrlParam } from '../index'
 
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(10, 2)).toBe(12);
@@ -9,4 +9,18 @@ test('obj hasKey', () => {
     bb: 66
   }
   expect(hasKey(obj, 'cc')).toBe(false);
+})
+test('getType', () => {
+  const obj = {}, arr = [], num = 1, str = "1", bool = false, isNull = null, isUndefined = undefined;
+  expect(getType(obj)).toBe('Object');
+  expect(getType(arr)).toBe('Array');
+  expect(getType(num)).toBe('Number');
+  expect(getType(str)).toBe('String');
+  expect(getType(bool)).toBe('Boolean');
+  expect(getType(isNull)).toBe('Null');
+  expect(getType(isUndefined)).toBe('Undefined');
+})
+
+test('getUrlParam', () => {
+  expect(getUrlParam('pid')).toBe('37289');
 })
