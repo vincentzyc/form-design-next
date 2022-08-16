dbcb59049c086678fa8b837289db0fd9d0e615d6
vincentzyc <vincentzyc@qq.com>
2022-08-16 08:57:02 +0800

    🧪 test: jest test

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,7 @@
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
+  collectCoverage: true,
+  collectCoverageFrom: ["**/*.{js,vue}", "!**/node_modules/**"],
   testEnvironmentOptions: {
     url: 'https://h5.junjue888.com/h5/index.html?id=6061699948802140225&abtest=1&pid=37289'
   }
