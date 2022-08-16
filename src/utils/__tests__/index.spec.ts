c0ceeaba237ff09ade090cc39540909c8b7a5105
vincentzyc <vincentzyc@qq.com>
2022-08-16 11:36:38 +0800

    🧪 test: jest test

--- src/utils/__tests__/index.spec.ts
+++ src/utils/__tests__/index.spec.ts
@@ -1,26 +1,60 @@
-import { sum, hasKey, getType, getUrlParam } from '../index'
+import * as util from '../index'
 
 test('adds 1 + 2 to equal 3', () => {
-  expect(sum(10, 2)).toBe(12);
+  expect(util.sum(10, 2)).toBe(12);
 });
-test('obj hasKey', () => {
+test('hasKey', () => {
   const obj = {
     aa: 33,
     bb: 66
   }
-  expect(hasKey(obj, 'cc')).toBe(false);
-})
-test('getType', () => {
-  const obj = {}, arr = [], num = 1, str = "1", bool = false, isNull = null, isUndefined = undefined;
-  expect(getType(obj)).toBe('Object');
-  expect(getType(arr)).toBe('Array');
-  expect(getType(num)).toBe('Number');
-  expect(getType(str)).toBe('String');
-  expect(getType(bool)).toBe('Boolean');
-  expect(getType(isNull)).toBe('Null');
-  expect(getType(isUndefined)).toBe('Undefined');
+  expect(util.hasKey(obj, 'cc')).toBe(false);
 })
 
+test('getNanoid', () => {
+  expect(util.getNanoid()).toHaveLength(21);
+})
+
+test('getType', () => {
+  const obj = {}, arr = [], num = 1, str = "1", bool = false, isNull = null, isUndefined = undefined;
+  expect(util.getType(obj)).toBe('Object');
+  expect(util.getType(arr)).toBe('Array');
+  expect(util.getType(num)).toBe('Number');
+  expect(util.getType(str)).toBe('String');
+  expect(util.getType(bool)).toBe('Boolean');
+  expect(util.getType(isNull)).toBe('Null');
+  expect(util.getType(isUndefined)).toBe('Undefined');
+})
+
+test('isDef', () => {
+  expect(util.isDef('')).toBe(true);
+  expect(util.isDef('0')).toBe(true);
+  expect(util.isDef('11')).toBe(true);
+  expect(util.isDef(0)).toBe(true);
+  expect(util.isDef(1)).toBe(true);
+  expect(util.isDef({})).toBe(true);
+  expect(util.isDef(null)).toBe(false);
+  expect(util.isDef(undefined)).toBe(false);
+})
+
+test('isObject', () => {
+  expect(util.isObject({})).toBe(true);
+  expect(util.isObject({ aa: '11' })).toBe(true);
+  expect(util.isObject('')).toBe(false);
+})
+
+test('isArray', () => {
+  expect(util.isArray([])).toBe(true);
+  expect(util.isArray([1, 2, 3])).toBe(true);
+  expect(util.isArray({})).toBe(false);
+})
+
+test('isString', () => {
+  expect(util.isString('')).toBe(true);
+  expect(util.isString(0)).toBe(false);
+})
+
+
 test('getUrlParam', () => {
-  expect(getUrlParam('pid')).toBe('37289');
+  expect(util.getUrlParam('pid')).toBe('37289');
 })
