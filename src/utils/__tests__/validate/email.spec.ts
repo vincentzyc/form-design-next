4ebbc054cdff15bb817c6745227b73253546a4ff
vincentzyc <vincentzyc@qq.com>
2022-08-22 10:18:37 +0800

    🧪 test: jest test

--- src/utils/__tests__/validate/email.spec.ts
+++ src/utils/__tests__/validate/email.spec.ts
@@ -5,5 +5,6 @@ test('isEmail', () => {
   expect(isEmail(' abc@gmail.com ')).toBeTruthy();
   expect(isEmail('abc@@gmail.com')).toBeFalsy();
   expect(isEmail('@gmail.com')).toBeFalsy();
+  expect(isEmail('aa@bb.cc')).toBeTruthy();
   expect(isEmail('abc@')).toBeFalsy();
 });
