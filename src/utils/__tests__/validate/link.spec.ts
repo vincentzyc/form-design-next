a21b5fd46ca4e756c6f2340564a0fc7270310e8d
vincentzyc <vincentzyc@qq.com>
2022-08-22 10:15:33 +0800

    🧪 test: jest test

--- src/utils/__tests__/validate/link.spec.ts
+++ src/utils/__tests__/validate/link.spec.ts
@@ -1,7 +1,14 @@
-export function isLink(value: string): boolean {
-  if (typeof value !== 'string') return false
-  if (value.includes('weixin://')) return true
-  const pattern = /^(((ht|f)tps?):\/\/)?[\w-]+(\.[\w-]+)+([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?$/
-  const isLink = pattern.test(value);
-  return isLink;
-}
+
+import { isLink } from '../../validate/link'
+
+test('isEmail', () => {
+  expect(isLink('weixin://xxx')).toBeTruthy();
+  expect(isLink('abc@gmail.com')).toBeFalsy();
+  expect(isLink('192.168.1.1')).toBeFalsy();
+  expect(isLink('https://www.baidu.com')).toBeTruthy();
+  expect(isLink('//www.baidu.com')).toBeFalsy();
+  expect(isLink('www.baidu.com')).toBeTruthy();
+  expect(isLink('http:xxx.abc')).toBeFalsy();
+  expect(isLink('http://xxx.abc')).toBeTruthy();
+  expect(isLink('ftp://xxx.abc')).toBeTruthy();
+});
