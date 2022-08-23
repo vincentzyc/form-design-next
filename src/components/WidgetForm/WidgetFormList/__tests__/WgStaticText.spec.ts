b4292529cf9c5d54246e17e3212739722c543af7
vincentzyc <vincentzyc@qq.com>
2022-08-24 01:01:05 +0800

    🧪 test: jest test

--- src/components/WidgetForm/WidgetFormList/__tests__/WgStaticText.spec.ts
+++ src/components/WidgetForm/WidgetFormList/__tests__/WgStaticText.spec.ts
@@ -1,7 +1,7 @@
 import { mount } from '@vue/test-utils'
 import WgStaticText from '../WgStaticText'
 
-const item = {
+const item1 = {
   type: "StaticText",
   name: "文本描述",
   value: "这是一段文本",
@@ -15,15 +15,19 @@ const item = {
   }
 }
 
+const item2 = {
+  ...item1,
+  value: '这是测试修改文本'
+}
+
 test('WgStaticText.vue', async () => {
   const wrapper = mount(WgStaticText, {
-    props: { item }
+    props: { item: item1 }
   })
   expect(wrapper.html()).toContain('这是一段文本')
   expect(wrapper.html()).toContain('margin: 0px 0px 0px 0px;');
-  
-  item.value = '这是测试修改文本'
-  await wrapper.setProps({ item });
+
+  await wrapper.setProps({ item: item2 });
 
   expect(wrapper.html()).toContain('这是测试修改文本')
 })
