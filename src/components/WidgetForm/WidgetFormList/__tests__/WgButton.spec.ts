fd1d74e5ff6e34aaa7fd0edb9aa0c257dc5a3978
vincentzyc <vincentzyc@qq.com>
2022-08-31 10:22:00 +0800

    🧪 test: jest test

---
+++ src/components/WidgetForm/WidgetFormList/__tests__/WgButton.spec.ts
@@ -0,0 +1,49 @@
+import { mount } from '@vue/test-utils'
+import WgButton from '../WgButton'
+
+const item = {
+  type: "Button",
+  name: "按钮",
+  btnText: "提交",
+  btnType: "submit",
+  apiKey: "",
+  positionFixed: 'auto',
+  fixedTypes: ['auto', 'top', 'bottom'], // auto--正常 top--顶部悬浮  custom--自定义位置悬浮  bottom--底部悬浮
+  scrollHeight: 0,
+  btnTypes: [{
+    value: "submit",
+    label: "提交"
+  }],
+  style: {
+    margin: "10px 10px 10px 10px",
+    padding: "0px 0px 0px 0px",
+    isImgBtn: false,
+    value: "",
+    btnStyle: {
+      borderRadius: '20px',
+      borderradius: 20,
+      backgroundColor: "#409EFF",
+      color: "#fff"
+    }
+  },
+  animation: {
+    animationName: '',
+    animationduration: 2,
+    animationDuration: '2s',
+    animationIterationCount: 'infinite',
+    animationTimingFunction: 'linear',
+    className: ''
+  }
+}
+
+test('WgImgShow.vue', async () => {
+  const wrapper = mount(WgButton, {
+    props: { item }
+  })
+
+  expect(wrapper.html()).toContain(item.btnText)
+  // expect(wrapper.find('p').attributes('style')).toContain('color: rgb(51, 51, 51)')
+  // expect(wrapper.findAll('.agreement-item')).toHaveLength(item.titleTexts.length)
+  // await wrapper.setProps({ item: { ...item, tipText: "点击提交表示同意" } });
+  // expect(wrapper.html()).toContain(item.tipText)
+})
