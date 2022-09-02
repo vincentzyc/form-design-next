c71e91c98e7fbf8f0409d4e42fca3712ce03b1d4
vincentzyc <vincentzyc@qq.com>
2022-09-02 10:28:30 +0800

    🧪 test: jest test

--- src/components/WidgetForm/WidgetFormList/WgSelect.tsx
+++ src/components/WidgetForm/WidgetFormList/WgSelect.tsx
@@ -13,7 +13,7 @@ export default defineComponent({
     const wrapClass = computed(() => ['wg-item wg-padding', item.label.labelPosition === 'top' ? 'flex-column' : 'align-middle'])
     return () => (
       <div class={wrapClass.value} style={item.style}>
-        <div class="wg-title" style={{ width: item.label.labelWidth }} v-show={item.showLabel}>{item.label.labelTitle}</div>
+        <div class="wg-title" style={{ width: item.label.labelWidth }} v-show={props.item.showLabel}>{item.label.labelTitle}</div>
         <div class="flex-auto">
           <select v-model={item.value} class="wg-select flex-auto">
             <option value="" disabled selected hidden>{item.placeholder}</option>
