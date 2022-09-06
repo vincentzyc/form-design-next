2b180486fbe0fc02eae21a184973918253fc622b
vincentzyc <vincentzyc@qq.com>
2022-09-06 11:07:12 +0800

    🧪 test: jest test

--- src/components/WidgetForm/WidgetFormList/WgMarquee.tsx
+++ src/components/WidgetForm/WidgetFormList/WgMarquee.tsx
@@ -1,4 +1,4 @@
-import { defineComponent, reactive, computed, ref, onMounted, nextTick } from "vue";
+import { defineComponent, computed, ref, onMounted, nextTick } from "vue";
 
 export default defineComponent({
   name: "WgMarquee",
@@ -9,31 +9,30 @@ export default defineComponent({
     }
   },
   setup(props) {
-    const { item } = reactive(props)
     const marqueeWidth = ref(0)
     const wgMarquee = ref()
-    const wrapClass = computed(() => ['wg-marquee', { 'flex align-middle': item.direction === 'left' }])
+    const wrapClass = computed(() => ['wg-marquee', { 'flex align-middle': props.item.direction === 'left' }])
 
     const imgStyle = computed(() => ({
       width: marqueeWidth.value + 'px',
-      height: item.direction === 'left' ? item.style.height : 'auto'
+      height: props.item.direction === 'left' ? props.item.style.height : 'auto'
     }))
-    const inlineClass = computed(() => item.direction === 'left' ? ['wg-marquee-left', 'flex-inline'] : ['wg-marquee-up'])
+    const inlineClass = computed(() => props.item.direction === 'left' ? ['wg-marquee-left', 'flex-inline'] : ['wg-marquee-up'])
 
     const renderImg = () => (
       <div>
-        {item.imgUrl ?
-          <img src={item.imgUrl} alt="图片" style={imgStyle.value} /> :
+        {props.item.imgUrl ?
+          <img src={props.item.imgUrl} alt="图片" style={imgStyle.value} /> :
           <img src={require('@/assets/img/img-placeholder.png')} alt="图片" style={imgStyle.value} />}
-        {item.imgUrl ?
-          <img src={item.imgUrl} alt="图片" style={imgStyle.value} /> :
+        {props.item.imgUrl ?
+          <img src={props.item.imgUrl} alt="图片" style={imgStyle.value} /> :
           <img src={require('@/assets/img/img-placeholder.png')} alt="图片" style={imgStyle.value} />}
       </div>
     )
 
     const renderText = () => (
       <div>{
-        [...item.textList, ...item.textList].map((texts, key) => (
+        [...props.item.textList, ...props.item.textList].map((texts, key) => (
           <p key={key} class="flex space-around" style={{ width: marqueeWidth.value + 'px' }}>
             {texts.split(/\s+/).map((text: string, i: number) => <span key={i}>{text}</span>)}
           </p>
@@ -47,12 +46,12 @@ export default defineComponent({
     })
 
     return () => (
-      <div style={item.style} class={wrapClass.value} ref={wgMarquee}>
+      <div style={props.item.style} class={wrapClass.value} ref={wgMarquee}>
         <div
           class={inlineClass}
-          style={{ animation: `marquee-${item.direction} ${item.durationTime}s linear infinite` }}
+          style={{ animation: `marquee-${props.item.direction} ${props.item.durationTime}s linear infinite` }}
         >
-          {item.isImgBtn ? renderImg() : renderText()}
+          {props.item.isImgBtn ? renderImg() : renderText()}
         </div>
       </div>
     )
