b81ef119ff6c09b7ccac32fe64b56de5f090e753
vincentzyc <vincentzyc@qq.com>
2023-05-27 22:09:29 +0800

    ✨feat: 新增自定义组件

---
+++ src/assets/js/widget/CustomWidget/CustomWidget.ts
@@ -0,0 +1,6 @@
+export default [
+  {
+    type: 'CustomWidget',
+    name: '自定义组件',
+  },
+];
---
+++ src/assets/js/widget/CustomWidget/index.ts
@@ -0,0 +1,7 @@
+import CustomWidget from "./CustomWidget"
+
+export default [{
+  name: "自定义组件",
+  value: "customWidget",
+  data: CustomWidget
+}]
