77d6173a661effbaf1769cf63a7542d8e272e044
vincentzyc <vincentzyc@qq.com>
2023-05-29 11:09:39 +0800

    ✨feat: 新增自定义组件

--- src/components/PageConfig/WidgetConfig/CustomWidget.vue
+++ src/components/PageConfig/WidgetConfig/CustomWidget.vue
@@ -13,116 +13,124 @@
   </el-tooltip>
 </template>
 
-<script>
-const CustomWidgetsKey = 'CustomWidgets';
+<script lang="ts" setup>
+import { getLocalStorage, setLocalStorage } from '@/utils/storage';
+import { ElMessage, ElMessageBox } from 'element-plus';
+import { computed } from 'vue';
 
-export default {
-  props: {
-    pageData: {
-      type: Object,
-      required: true,
-    },
-  },
-  computed: {
-    disabledBtn() {
-      if (Array.isArray(this.pageData.list) && this.pageData.list.length > 0) {
-        const hadSticky = this.pageData.list.some(v => v.positionSticky);
-        if (hadSticky) return true;
-        if (Array.isArray(this.pageData.fixedBottom) && this.pageData.fixedBottom.length > 0) return true;
-        if (Array.isArray(this.pageData.fixedCustom) && this.pageData.fixedCustom.length > 0) return true;
-        return false;
-      } else {
-        return true;
-      }
-    },
-  },
-  methods: {
-    handleCustomWidget() {
-      this.$root.$emit('createh5_savePage');
-      if (Array.isArray(this.pageData.list) && this.pageData.list.length > 0) {
-        this.$prompt('请输入自定义组件名称', '提示', {
-          inputPlaceholder: '支持输入中文、英文、数字、横杠和下划线,长度不超过20',
-          showClose: false,
-          inputPattern: /^[\u4e00-\u9fa5a-zA-Z0-9_-]{1,20}$/,
-          inputErrorMessage: '格式不正确,支持中文、英文、数字、横杠和下划线,长度不超过20',
-        })
-          .then(({ value }) => {
-            this.saveCustomWidget(value);
-          })
-          .catch(() => {});
-      } else {
-        this.$message({
-          type: 'error',
-          message: '请拖入正常显示位置的组件，自定义、底部悬浮、吸顶悬浮的组件不支持',
-        });
-      }
-    },
-    saveCustomWidget(name) {
-      const industry = this.pageData.industry;
-      const customWidgetItem = {
-        name: name,
-        customList: this.pageData.list,
-      };
-      this.save2Local(customWidgetItem, industry);
-    },
-    deleteLocalWg(industry, index) {
-      let customWidgets = this.$util.getLStorage(CustomWidgetsKey);
-      if (customWidgets) {
-        const industryCustomWidgets = customWidgets[industry];
-        if (Array.isArray(industryCustomWidgets) && industryCustomWidgets.length > 0) {
-          industryCustomWidgets.splice(index, 1);
-          this.$util.setLStorage(CustomWidgetsKey, customWidgets);
-          return true;
-        } else {
-          return false;
-        }
-      }
-      return false;
-    },
-    async save2Local(customWidgetItem, industry) {
-      let customWidgets = this.$util.getLStorage(CustomWidgetsKey);
-      if (customWidgets) {
-        const industryCustomWidgets = customWidgets[industry];
-        if (Array.isArray(industryCustomWidgets) && industryCustomWidgets.length > 0) {
-          const newWgs = await this.uniWidgets(industryCustomWidgets, customWidgetItem);
-          if (newWgs) {
-            customWidgets[industry] = newWgs;
-          } else {
-            return;
-          }
-        } else {
-          customWidgets[industry] = [customWidgetItem];
-        }
-      } else {
-        customWidgets = {
-          [industry]: [customWidgetItem],
-        };
-      }
-      this.$util.setLStorage(CustomWidgetsKey, customWidgets);
-      this.$message({
-        type: 'success',
-        message: '保存成功!',
-      });
-      this.$emit('success');
-    },
-    uniWidgets(industryCustomWidgets, customWidgetItem) {
-      const wgsIndex = industryCustomWidgets.findIndex(v => v.name === customWidgetItem.name);
-      if (wgsIndex > -1) {
-        return new Promise(resolve => {
-          this.$confirm('已存在相同名称的自定义组件, 是否覆盖?', '提示', {
-            type: 'warning',
-          })
-            .then(() => {
-              industryCustomWidgets[wgsIndex] = customWidgetItem;
-              resolve(industryCustomWidgets);
-            })
-            .catch(() => resolve(false));
-        });
-      } else {
-        industryCustomWidgets.push(customWidgetItem);
-        return industryCustomWidgets;
-      }
-    },
-  },
+type TypeCustomWidgetItem = {
+  name: string;
+  customList: Record<string, any>;
 };
+
+const CustomWidgetsKey = 'CustomWidgets';
+const props = defineProps({
+  pageData: {
+    required: true,
+    type: Object,
+    default: () => ({}),
+  },
+});
+const emit = defineEmits(['success']);
+
+defineExpose({ deleteLocalWg });
+
+const disabledBtn = computed(() => {
+  if (Array.isArray(props.pageData.list) && props.pageData.list.length > 0) {
+    const hadSticky = props.pageData.list.some(v => v.positionSticky);
+    if (hadSticky) return true;
+    if (Array.isArray(props.pageData.fixedBottom) && props.pageData.fixedBottom.length > 0) return true;
+    if (Array.isArray(props.pageData.fixedCustom) && props.pageData.fixedCustom.length > 0) return true;
+    return false;
+  } else {
+    return true;
+  }
+});
+
+function handleCustomWidget() {
+  // this.$root.$emit('createh5_savePage');
+  if (Array.isArray(props.pageData.list) && props.pageData.list.length > 0) {
+    ElMessageBox.prompt('请输入自定义组件名称', '提示', {
+      inputPlaceholder: '支持输入中文、英文、数字、横杠和下划线,长度不超过20',
+      showClose: false,
+      inputPattern: /^[\u4e00-\u9fa5a-zA-Z0-9_-]{1,20}$/,
+      inputErrorMessage: '格式不正确,支持中文、英文、数字、横杠和下划线,长度不超过20',
+    })
+      .then(({ value }) => {
+        saveCustomWidget(value);
+      })
+      .catch();
+  } else {
+    ElMessage({
+      type: 'error',
+      message: '请拖入正常显示位置的组件，自定义、底部悬浮、吸顶悬浮的组件不支持',
+    });
+  }
+}
+function saveCustomWidget(name: string) {
+  const industry = props.pageData.industry;
+  const customWidgetItem: TypeCustomWidgetItem = {
+    name: name,
+    customList: props.pageData.list,
+  };
+  save2Local(customWidgetItem, industry);
+}
+function deleteLocalWg(industry: string, index: number) {
+  let customWidgets = getLocalStorage(CustomWidgetsKey);
+  if (customWidgets) {
+    const industryCustomWidgets = customWidgets[industry];
+    if (Array.isArray(industryCustomWidgets) && industryCustomWidgets.length > 0) {
+      industryCustomWidgets.splice(index, 1);
+      setLocalStorage(CustomWidgetsKey, customWidgets);
+      return true;
+    } else {
+      return false;
+    }
+  }
+  return false;
+}
+async function save2Local(customWidgetItem: TypeCustomWidgetItem, industry: string) {
+  let customWidgets = getLocalStorage(CustomWidgetsKey);
+  if (customWidgets) {
+    const industryCustomWidgets: TypeCustomWidgetItem[] = customWidgets[industry];
+    if (Array.isArray(industryCustomWidgets) && industryCustomWidgets.length > 0) {
+      const newWgs = await uniWidgets(industryCustomWidgets, customWidgetItem);
+      if (newWgs) {
+        customWidgets[industry] = newWgs;
+      } else {
+        return;
+      }
+    } else {
+      customWidgets[industry] = [customWidgetItem];
+    }
+  } else {
+    customWidgets = {
+      [industry]: [customWidgetItem],
+    };
+  }
+  setLocalStorage(CustomWidgetsKey, customWidgets);
+  ElMessage({
+    type: 'success',
+    message: '保存成功!',
+  });
+  emit('success');
+}
+function uniWidgets(industryCustomWidgets: TypeCustomWidgetItem[], customWidgetItem: TypeCustomWidgetItem) {
+  const wgsIndex = industryCustomWidgets.findIndex(v => v.name === customWidgetItem.name);
+  if (wgsIndex > -1) {
+    return new Promise(resolve => {
+      ElMessageBox.confirm('已存在相同名称的自定义组件, 是否覆盖?', '提示', {
+        type: 'warning',
+      })
+        .then(() => {
+          industryCustomWidgets[wgsIndex] = customWidgetItem;
+          resolve(industryCustomWidgets);
+        })
+        .catch(() => resolve(false));
+    });
+  } else {
+    industryCustomWidgets.push(customWidgetItem);
+    return industryCustomWidgets;
+  }
+}
 </script>
