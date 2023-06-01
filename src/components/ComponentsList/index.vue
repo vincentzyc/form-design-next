fe3816d51475a46b9feda2ed8902a96dada764e7
vincentzyc <vincentzyc@qq.com>
2023-06-01 09:15:38 +0800

    ✨feat: 新增自定义组件

--- src/components/ComponentsList/index.vue
+++ src/components/ComponentsList/index.vue
@@ -14,23 +14,25 @@
     <div class="flex-auto components-content">
       <ul :key="level1.value" v-for="level1 in widgetLevel1" v-show="level1.value === widgetLevel2.value">
         <li :key="level2.value" v-for="level2 in level1.data">
-          <template v-if="level2.value === 'customWidget'">
+          <template v-if="level2.value === CustomWidget">
             <template v-if="customWidgets && customWidgets.length > 0">
-              <p class="cred text-center">目前只对当前浏览器生效，其它浏览器需重新添加</p>
               <Draggable
                 :clone="customWidgetsClone"
                 :group="{ name: 'widget', pull: 'clone', put: false }"
-                :list="customWidgets"
                 :sort="false"
                 @end="dragEnd"
-                filter=".disdraggable"
                 ghostClass="ghost"
+                item-key="name"
                 tag="ul"
+                v-model="customWidgets"
               >
-                <li v-for="(customWg, index) in customWidgets" :key="customWg.name" class="form-edit-widget-label">
-                  {{ customWg.name }}
-                  <i @click.stop="deleteCustomWg(index, customWg.name)" class="el-icon-close wg-close-icon"></i>
-                </li>
+                <template #item="{ index, element }">
+                  <li class="form-edit-widget-label">
+                    {{ element.name }}
+                    {{ index }}
+                    <!-- <i @click.stop="deleteCustomWg(index, element.name)" class="el-icon-close wg-close-icon"></i> -->
+                  </li>
+                </template>
               </Draggable>
             </template>
             <p v-else class="text-center"> 空空如也 </p>
@@ -66,8 +68,7 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import { getLocalStorage } from '@/utils/storage';
-import { onCustomWidgetsSave } from '@/bus';
+
 export default defineComponent({
   name: 'ComponentsList',
 });
@@ -79,10 +80,12 @@ import Draggable from 'vuedraggable';
 import { getNanoid, hasKey } from '@/utils/index';
 import { deepClone } from '@/utils/deep-clone';
 import widgetLevel1 from '@/assets/js/widget';
+import { CustomWidget } from '@/assets/js/widget/widgets-types';
 import { useMainStore } from '@/pinia';
 import { storeToRefs } from 'pinia';
-import { ElMessage, ElMessageBox } from 'element-plus';
-import { customWidgetsCloneData } from '@/assets/js/pageDataUtil';
+// import { ElMessage, ElMessageBox } from 'element-plus';
+import { getLocalStorage } from '@/utils/storage';
+import { onCustomWidgetsSave } from '@/bus';
 
 const CustomWidgetsKey = 'CustomWidgets';
 
@@ -98,40 +101,34 @@ const widgetLevel2 = ref(widgetLevel1[0]);
 
 const customWidgets = ref<TypeCustomWidgetItem[]>([]);
 
-function customWidgetsClone(obj: Record<string, unknown>) {
-  const newObj = customWidgetsCloneData(obj);
-  mainStore.setDragWg(newObj);
-  return newObj;
-}
-
-function deleteCustomWg(index: number, name: string) {
-  ElMessageBox.confirm('确认删除自定义组件' + name, '提示', {
-    type: 'warning',
-  })
-    .then(() => {
-      // if (this.$refs.customWidget) {
-      //   const deleteRes = this.$refs.customWidget.deleteLocalWg(this.pageData.industry, index);
-      //   if (deleteRes) {
-      customWidgets.value.splice(index, 1);
-      ElMessage({
-        type: 'success',
-        message: '删除成功!',
-      });
-      //   } else {
-      //     ElMessage({
-      //       type: 'error',
-      //       message: '删除失败',
-      //     });
-      //   }
-      // }
-    })
-    .catch(() => null);
-}
+// function deleteCustomWg(index: number, name: string) {
+//   ElMessageBox.confirm('确认删除自定义组件' + name, '提示', {
+//     type: 'warning',
+//   })
+//     .then(() => {
+//       // if (this.$refs.customWidget) {
+//       //   const deleteRes = this.$refs.customWidget.deleteLocalWg(this.pageData.industry, index);
+//       //   if (deleteRes) {
+//       customWidgets.value.splice(index, 1);
+//       ElMessage({
+//         type: 'success',
+//         message: '删除成功!',
+//       });
+//       //   } else {
+//       //     ElMessage({
+//       //       type: 'error',
+//       //       message: '删除失败',
+//       //     });
+//       //   }
+//       // }
+//     })
+//     .catch(() => null);
+// }
 
 function getLocalCustomWidgets() {
-  let customWidgets = getLocalStorage(CustomWidgetsKey);
-  if (customWidgets) {
-    customWidgets.value = customWidgets;
+  let localCustomWidgets = getLocalStorage(CustomWidgetsKey);
+  if (localCustomWidgets) {
+    customWidgets.value = localCustomWidgets;
   }
 }
 
@@ -152,7 +149,27 @@ function disFormList(wgItem: Record<string, unknown>) {
   }
   return false;
 }
+/**
+ * 更新组件key
+ * @param {Array} arr 组件列表
+ * @returns 组件列表
+ */
+ function updateWgKey(arr) {
+  arr.forEach(wg => {
+    if (wg.key) wg.key = wg.type + '_' + getNanoid();
+    if (wg.list) wg.list = updateWgKey(wg.list);
+    if (wg.popupList) wg.popupList = updateWgKey(wg.popupList);
+  });
+  return arr;
+}
 
+function customWidgetsClone(obj: Record<string, unknown>) {
+  obj.customList = updateWgKey(obj.customList);
+  obj.type = CustomWidget;
+  const newObj = deepClone(obj);
+  mainStore.setDragWg(newObj);
+  return newObj;
+}
 function cloneData(obj: Record<string, unknown>) {
   const newObj = deepClone(obj);
   newObj.key = newObj.type + '_' + getNanoid();
@@ -161,6 +178,7 @@ function cloneData(obj: Record<string, unknown>) {
 }
 
 onMounted(() => {
+  getLocalCustomWidgets();
   onCustomWidgetsSave(() => getLocalCustomWidgets());
 });
 </script>
