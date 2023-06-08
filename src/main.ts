8e1cdbf2b1ae1f0b2d7f9ee0e8923308cc7457c5
vincentzyc <vincentzyc@qq.com>
2023-06-08 15:42:21 +0800

    🐳chore: add quill

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,25 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
+import { createApp } from 'vue';
+import App from './App.vue';
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css';
 import mitt from 'mitt';
-import { createPinia } from 'pinia'
+import { createPinia } from 'pinia';
 
-import '@/assets/css/base.css'
+import '@/assets/css/base.css';
 import '@/assets/css/index.styl';
 import '@/assets/css/widget.styl';
 import '@/assets/css/config.styl';
 import '@/assets/css/themes.styl';
 
-const app = createApp(App)
-app.use(createPinia())
+const app = createApp(App);
+app.use(createPinia());
 
-app.use(ElementPlus)
+app.use(ElementPlus);
 
 const emitter = mitt();
 
-app.config.globalProperties.$bus = emitter
+app.config.globalProperties.$bus = emitter;
 
-app.config.globalProperties.BASE_URL = process.env.BASE_URL
+app.config.globalProperties.BASE_URL = process.env.BASE_URL as any;
 
-app.mount('#app')
+app.mount('#app');
