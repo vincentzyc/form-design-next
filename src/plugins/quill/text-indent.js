f208ae0b572045f4a659a251a2c41e0f23e58b6d
vincentzyc <vincentzyc@qq.com>
2023-06-15 10:38:57 +0800

    🐳chore: add quill

--- src/plugins/quill/text-indent.js
+++ src/plugins/quill/text-indent.js
@@ -1,19 +1,8 @@
 import Quill from 'quill';
 const Parchment = Quill.import('parchment');
 
-class IndentStyleAttributor extends Parchment.Attributor.Style {
-  // value(domNode) {
-  //   let value = super.value(domNode);
-  //   return value;
-  // }
-  // add(node,value) {
-  //   node.style.textIndent = value
-  //   return true;
-  // }
-  // remove(node) {
-  //   node.style.textIndent = '';
-  // }
-}
+class IndentStyleAttributor extends Parchment.Attributor.Style {}
+
 const IndentStyle = new IndentStyleAttributor('textIndent', 'text-indent', {
   scope: Parchment.Scope.BLOCK,
   whitelist: ['0em', '1em', '2em', '3em', '4em', '5em'],
