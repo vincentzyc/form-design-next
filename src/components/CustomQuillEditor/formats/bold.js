fa876182145df1eb2982ab2919592e8922151026
vincentzyc <vincentzyc@qq.com>
2023-06-24 14:35:32 +0800

    🐳chore: add quill

--- src/components/CustomQuillEditor/formats/bold.js
+++ src/components/CustomQuillEditor/formats/bold.js
@@ -2,21 +2,22 @@ import Quill from 'quill';
 const Parchment = Quill.import('parchment');
 
 class BoldStyleAttributor extends Parchment.Attributor.Style {
-  value(node) {
-    return node.style.fontWeight === 'bold' ? true : undefined;
+  value(domNode) {
+    let value = super.value(domNode);
+    console.log(value);
+    return domNode.style.fontWeight === 'bold' ? true : undefined;
   }
-
-  add(node, value) {
-    if (value) {
-      node.style.fontWeight = 'bold';
-    } else {
-      node.style.fontWeight = '';
-    }
-  }
+  //   add(node, value) {
+  //     if (value) {
+  //       node.style.fontWeight = 'bold';
+  //     } else {
+  //       node.style.fontWeight = '';
+  //     }
+  //   }
 }
 
 const BoldStyle = new BoldStyleAttributor('bold', 'font-weight', {
   scope: Parchment.Scope.INLINE,
+  whitelist: ['normal', 'bold'],
 });
-
 export default BoldStyle;
